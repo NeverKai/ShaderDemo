472a6de6c33023de90417d8d4ea334625780f25d
KZ <115853593@qq.com>
2021-11-06 16:26:48 +0800

    脚本

---
+++ ShaderDemo/Assets/CameraAlignView.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CameraAlignView : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnRenderObject()
+    {
+        transform.position = SceneView.lastActiveSceneView.camera.transform.position;
+        transform.rotation = SceneView.lastActiveSceneView.camera.transform.rotation;
+    }
+}
